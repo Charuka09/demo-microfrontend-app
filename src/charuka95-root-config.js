af0736eb60e3425e0b006daf0f5b189f84da0d94
Charuka Hearth <charukahome@Charukas-MacBook-Pro.local>
2021-02-17 02:43:35 +0530

    initial commit for demo-app-root-config

---
+++ src/charuka95-root-config.js
@@ -0,0 +1,32 @@
+import { registerApplication, start } from "single-spa";
+
+registerApplication({
+  name: "@single-spa/welcome",
+  app: () =>
+    System.import(
+      "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
+    ),
+  activeWhen: ["/"],
+});
+
+registerApplication({
+  name: "@charuka95/spa-demo-home",
+  app: () => System.import("@charuka95/spa-demo-home"),
+  activeWhen: ["/"]
+});
+
+registerApplication({
+  name: "@charuka95/header",
+  app: () => System.import("@charuka95/header"),
+  activeWhen: ["/"]
+});
+
+registerApplication({
+  name: "@charuka95/test-page",
+  app: () => System.import("@charuka95/ttest-page"),
+  activeWhen: ["/"]
+});
+
+start({
+  urlRerouteOnly: true,
+});
